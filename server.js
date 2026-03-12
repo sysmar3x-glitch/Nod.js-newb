18c3f86292b83eed68c20508d7d36815919285e3
sysmar3x-glitch <sysmar3x@gmail.com>
2026-03-12 23:36:52 +0530

    fix backend save system

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("public"))
 
 const DB = "apps.json"
 
@@ -15,7 +14,7 @@ return JSON.parse(fs.readFileSync(DB))
 }
 
 function saveApps(data){
-fs.writeFileSync(DB,JSON.stringify(data,null,2))
+fs.writeFileSync(DB, JSON.stringify(data,null,2))
 }
 
 app.get("/api/apps",(req,res)=>{
@@ -23,34 +22,27 @@ res.json(readApps())
 })
 
 app.post("/api/apps",(req,res)=>{
+
 const apps = readApps()
 
 const newApp = {
-id:Date.now(),
-name:req.body.name,
-icon:req.body.icon,
-version:req.body.version,
-size:req.body.size,
-description:req.body.description,
-download:req.body.download
+id: Date.now(),
+name: req.body.name,
+icon: req.body.icon,
+version: req.body.version,
+size: req.body.size,
+description: req.body.description,
+download: req.body.download
 }
 
 apps.push(newApp)
-saveApps(apps)
-
-res.json({status:"ok"})
-})
-
-app.delete("/api/apps/:id",(req,res)=>{
-let apps = readApps()
-
-apps = apps.filter(a=>a.id != req.params.id)
 
 saveApps(apps)
 
-res.json({status:"deleted"})
+res.json({status:"saved",app:newApp})
+
 })
 
 app.listen(3000,()=>{
-console.log("server running")
+console.log("Server running")
 })
